931aeb7747989f6cd24c3d6180c25b70b8d14a2b
AleksandarNesovic <crazynorth203@gmail.com>
2018-07-30 13:28:48 +0200

    new commit

--- narudzbina/src/main/java/com/nesovic/narudzbina/cors/CORSFilter.java
+++ narudzbina/src/main/java/com/nesovic/narudzbina/cors/CORSFilter.java
@@ -32,7 +32,7 @@ public class CORSFilter implements Filter {
 	        System.out.println("CORSFilter HTTP Request: " + request.getMethod());
 	 
 	        // Authorize (allow) all domains to consume the content
-	        ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Origin", "*");
+	        ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Origin", "GET");
 	        ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Methods","GET, OPTIONS, HEAD, PUT, POST");
 	        ((HttpServletResponse) servletResponse).setHeader("Access-Control-Allow-Headers", "Origin,Accept,Access-Control-Request-Method, Access-Control-Request-Headers,Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With,*");
 	        ((HttpServletResponse) servletResponse).setHeader("Access-Control-Request-Headers", " Origin, X-Atmosphere-tracking-id, X-Atmosphere-Framework, X-Cache-Date, Content-Type, X-Atmosphere-Transport,  *");

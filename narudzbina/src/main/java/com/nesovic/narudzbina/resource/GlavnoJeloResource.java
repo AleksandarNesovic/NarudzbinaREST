b45b0960c75abd49f27410914bd9119c6b4e3a6c
AleksandarNesovic <crazynorth203@gmail.com>
2018-07-31 16:03:21 +0200

    ada

--- narudzbina/src/main/java/com/nesovic/narudzbina/resource/GlavnoJeloResource.java
+++ narudzbina/src/main/java/com/nesovic/narudzbina/resource/GlavnoJeloResource.java
@@ -53,7 +53,7 @@ public class GlavnoJeloResource {
 		GlavnoJelo glavnoJelo=glavno.insertGlavnoJelo(g);
 		String idJela=String.valueOf(glavnoJelo.getId_glj());
 		URI uri=uriInfo.getAbsolutePathBuilder().path(idJela).build();
-		return Response.created(uri).header("Access-Control-Allow-Origin", "*").entity(glavnoJelo).header("Access-Control-Allow-Methods", "POST").build();
+		return Response.created(uri).header("Access-Control-Allow-Origin", "*").entity(glavnoJelo).build();
 	}
 	@DELETE
 	@Path("/{id}")
